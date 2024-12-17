981bd7ab50dd1b65ed33d4ccfdb0ce516e8a3210
Lysenkov Yurii <lysenkovyura90@gmail.com>
2024-12-17 20:59:58 +0500

    Add Serializer

--- task/views.py
+++ task/views.py
@@ -1,3 +1,16 @@
+from rest_framework import generics, viewsets
 from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+#class TaskViewSet(viewsets.ModelViewSet)
+
+class TaskAPIView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer 
--- task_list/urls.py
+++ task_list/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from task.views import TaskAPIView, TaskAPIDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/task_list/', TaskAPIView.as_view()),
+    path('api/v1/taskdetail/<int:pk>/', TaskAPIDetailView.as_view())
 ]
